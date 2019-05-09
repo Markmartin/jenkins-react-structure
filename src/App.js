37d3c6c1bd34dee794090abc2dfacf6fb644be62
GuYue <944565923@qq.com>
2019-05-09 11:36:05 +0800

    远程部署测试sehll

--- src/App.js
+++ src/App.js
@@ -6,23 +6,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <p>jenkins auto build</p>
         <p>Move to site</p>
         <p>mv 替代 cp shell</p>
         <p>除了本体部署还添加了远程部署 远程部署文件夹remoteDir</p>
         <p>清除目标目录所有文件</p>
+        <p>删除远程目标目录，并进行部署</p>
+        <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
   );
