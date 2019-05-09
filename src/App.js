03e66a459d53b20ae0bb6dd98376ada57b9e37dc
GuYue <944565923@qq.com>
2019-05-09 11:23:53 +0800

    测试远程部署

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ function App() {
         <p>jenkins auto build</p>
         <p>Move to site</p>
         <p>mv 替代 cp shell</p>
+        <p>除了本体部署还添加了远程部署 远程部署文件夹remoteDir</p>
       </header>
     </div>
   );
