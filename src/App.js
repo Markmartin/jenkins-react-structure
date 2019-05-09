3bcb048835148f55bc84abd3b2945f0768e2b82b
GuYue <944565923@qq.com>
2019-05-09 13:51:37 +0800

    修改shell脚本

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
         <p>删除远程目标目录，并进行部署</p>
         <p>删除远程脚本删除所有文件操作</p>
         <p>MV改成cp不然远程传输为空</p>
+        <p>修改shell脚本</p>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
