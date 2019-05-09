5f7d74e5121153bec7a4e62471407fb9b7492d34
GuYue <v-huyasheng>
2019-05-09 15:41:59 +0800

    teststructure

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>jenkins auto build</p>
+        {/* <p>jenkins auto build</p>
         <p>Move to site</p>
         <p>mv 替代 cp shell</p>
         <p>除了本体部署还添加了远程部署 远程部署文件夹remoteDir</p>
@@ -16,7 +16,7 @@ function App() {
         <p>MV改成cp不然远程传输为空</p>
         <p>修改shell脚本</p>
         <p>修复了远程构建路径---重新构建尝试--修复root密码</p>
-        <p>构建失败</p>
+        <p>构建失败</p> */}
         <p>最终版本测试</p>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
