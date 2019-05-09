813b79d57d39722d7b931404e9785fc2fbd38c0c
GuYue <944565923@qq.com>
2019-05-09 14:24:18 +0800

    修复香港服务器的root权限

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         <p>删除远程脚本删除所有文件操作</p>
         <p>MV改成cp不然远程传输为空</p>
         <p>修改shell脚本</p>
-        <p>修复了远程构建路径---重新构建尝试</p>
+        <p>修复了远程构建路径---重新构建尝试--修复root密码</p>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
