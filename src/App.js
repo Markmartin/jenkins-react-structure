52bf16e00e8200684ca0924b6dfe4e7541d2459b
GuYue <944565923@qq.com>
2019-05-09 11:29:31 +0800

    清楚目标目录所有文件

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function App() {
         <p>Move to site</p>
         <p>mv 替代 cp shell</p>
         <p>除了本体部署还添加了远程部署 远程部署文件夹remoteDir</p>
+        <p>清除目标目录所有文件</p>
       </header>
     </div>
   );
